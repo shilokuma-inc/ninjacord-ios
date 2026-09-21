86de5c95db8155a5e51d518093605705b19b972e
Takumi Muraishi <mrs1669.develop@gmail.com>
2026-09-21 17:13:21 +0900

    [refactor] WebView を共通コンポーネントに切り出す
    PrivacyPolicyView 内の private な PrivacyPolicyWebView を Common/WebView.swift
    に移動し、URL を渡すだけで使える共通部品にした。
    
    併せて load を updateUIView から makeUIView に移し、View の再評価のたびに
    再読み込みされないようにしている。

--- NinjacordPackage/Sources/NinjacordFeature/Setting/PrivacyPolicyView.swift
+++ NinjacordPackage/Sources/NinjacordFeature/Setting/PrivacyPolicyView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import WebKit
 
 struct PrivacyPolicyView: View {
     let baseUrl: String = "https://shilokuma-inc.github.io"
@@ -17,23 +16,10 @@ struct PrivacyPolicyView: View {
 
     var body: some View {
         if let privacyPolicyUrl = URL(string: privacyPolicyUrlString) {
-            PrivacyPolicyWebView(url: privacyPolicyUrl)
+            WebView(url: privacyPolicyUrl)
                 .navigationTitle("プライバシーポリシー")
         } else {
             EmptyView()
         }
-    }
-}
-
-private struct PrivacyPolicyWebView: UIViewRepresentable {
-    let url: URL
-
-    func makeUIView(context: Context) -> WKWebView {
-        return WKWebView()
-    }
-
-    func updateUIView(_ uiView: WKWebView, context: Context) {
-        let request = URLRequest(url: url)
-        uiView.load(request)
     }
 }
